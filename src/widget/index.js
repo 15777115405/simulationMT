5606a3f2f4d0922fbb4e541b2d027a0f62b8025d
koala_wei <397919222@qq.com>
2017-09-28 14:41:47 +0800

    添加颜色接口文件

---
+++ src/widget/index.js
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) 2017-present, Liu Jinyong
+ * All rights reserved.
+ *
+ * https://github.com/huanxsd/MeiTuan  
+ * @flow
+ */
+
+//import Screen from './Screen'
+//import system from './system'
+//import tool from './tool'
+
+//export {Screen,system}
